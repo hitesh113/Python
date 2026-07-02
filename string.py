a77d082464fecd0577299aec55f43e55812f7140
Hitesh Nayak <hitesh.7149@kistcollege.edu.np>
2026-07-02 15:26:23 +0545

    Initial message

---
+++ string.py
@@ -0,0 +1,20 @@
+name = "Hitesh"
+l = len(name)
+print(l)
+
+# Slicing
+t = name[0:3]
+print(t)
+
+r = name[-4:-1]
+s = name[1:4]
+print(r)
+print(s)
+print(name[:5])
+print(name[0:])
+
+# Slicing with skip value
+
+print(name[1:6:2])
+print(name[1:5:2])
+print(name[1:6:5])
