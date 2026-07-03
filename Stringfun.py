9037658905c9d78c7e25382ac1617cbbb38c4a18
Hitesh Nayak <hitesh.7149@kistcollege.edu.np>
2026-07-03 22:13:15 +0545

    Initial message

--- Stringfun.py
+++ Stringfun.py
@@ -1,4 +1,16 @@
+# Strings Function
 
+name = "hitesh"
 
-name = "Hitesh"
 print(name.endswith("sh"))
+print(name.startswith("h"))
+print(name.capitalize())    #function should be case sensitive
+print(name.upper())    
+print(name.replace("hitesh", "nayak"))    
+
+text = ("I", "code", "python")
+result = (" ".join(text))
+print(result)
+
+word = input("Enter your name: ")
+print(f"Good Afternoon, {word}")
